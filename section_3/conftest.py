4c434d550b9a44e86d5f26f08d675bb6e7e25725
Kirill <kovalev.kir.dmitr@gmail.com>
2023-08-12 10:00:52 +0300

    update

--- section_3/conftest.py
+++ section_3/conftest.py
@@ -2,6 +2,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     # обработчик опции
@@ -9,15 +10,19 @@ def pytest_addoption(parser):
                      help="Choose browser: chrome or firefox")
 
 @pytest.fixture(scope="function")
-def browser(request):
+def browser(request, user_language=None):
     browser_name = request.config.getoption("browser_name")
     browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     browser.implicitly_wait(15)
